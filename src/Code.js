175994881bc4341deb2466e326078d9302eabb79
Fedosik <izhevsk2073@gmail.com>
2022-09-09 14:00:18 +0400

    feat: create code.js

---
+++ src/Code.js
@@ -0,0 +1,42 @@
+document.querySelector('.form__button').onclick = myClick;
+let count = 0;
+function myClick(){
+  const inputCode = document.querySelector('.inputText').value;
+  const outText = document.querySelector('.out');
+  const loadingLine = document.querySelector('.nendoveb-kubscupon');
+  document.querySelector('.inputText').value = '';
+
+  outText.innerHTML = "";
+
+  if (inputCode === "correctpassword"){
+    loadingLine.classList.remove('hidden');
+    setTimeout(function() {
+      outText.innerHTML = "Правильный код!";
+      setTimeout(function() {
+        document.querySelector('.correctAnswer').classList.remove('hidden');
+        document.querySelector('.form__container').classList.add('hidden');
+      }, 500)
+    }, 2500);
+  }
+  else if (inputCode === ''){
+    outText.innerHTML = "Вы ввели пустую строку!";
+  }
+  else {
+    loadingLine.classList.remove('hidden');
+    setTimeout(function() {
+      outText.innerHTML = "Неправильный код!";
+    }, 2500);
+    count++;
+  }
+
+  if(count === 3){
+    document.querySelector('.form__button').disabled = true;
+    setTimeout(function() {
+      outText.innerHTML = "Превышен лимит неправильных ответов! :("
+    }, 2500);
+  }
+
+  setTimeout(function() {
+    loadingLine.classList.add('hidden');
+  }, 2500);
+}
